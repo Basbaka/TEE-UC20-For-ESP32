2a080d52c66518b89abe50cdf35fc9420cebe031
Basbaka <33049022+Basbaka@users.noreply.github.com>
2021-01-26 13:31:52 +0700

    Update FTP_List_File.ino

--- examples/FTP/FTP_List_File/FTP_List_File.ino
+++ examples/FTP/FTP_List_File/FTP_List_File.ino
@@ -1,6 +1,4 @@
 #include "TEE_UC20.h"
-#include "SoftwareSerial.h"
-#include <AltSoftSerial.h>
 #include "internet.h"
 #include "File.h"
 #include "ftp.h"
@@ -20,8 +18,6 @@
 INTERNET net;
 UC_FILE file;
 FTP ftp;
-//SoftwareSerial mySerial(8, 9); // RX, TX
-AltSoftSerial mySerial;
 
 void debug(String data)
 {
@@ -29,8 +25,8 @@ void debug(String data)
 }
 void setup() 
 {
-  Serial.begin(9600);
-  gsm.begin(&mySerial,9600);
+  Serial.begin(115200);
+  gsm.begin(&Serial2,115200);
   gsm.Event_debug = debug;
   Serial.println(F("UC20"));
   gsm.PowerOn(); 
