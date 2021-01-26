1aa8eeae3586eaf04ee4f59d0ee33a322a7f5cd5
Basbaka <33049022+Basbaka@users.noreply.github.com>
2021-01-26 14:54:15 +0700

    Update Ex_LineNotify.ino

--- examples/LineNotify/Ex_LineNotify/Ex_LineNotify.ino
+++ examples/LineNotify/Ex_LineNotify/Ex_LineNotify.ino
@@ -1,6 +1,6 @@
-#include "TEE_UC20.h"
-#include "internet.h"
-#include "LineNotify.h"
+#include "esp32_TEE_UC20.h"
+#include "esp32_internet.h"
+#include "esp32_LineNotify.h"
 
 #define APN "internet"
 #define USER ""
